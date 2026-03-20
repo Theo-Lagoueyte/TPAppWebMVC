036e3a99a4ee5c0bb522d6292ea9b16b689ebfed
Theo-Lagoueyte <154439816+Theo-Lagoueyte@users.noreply.github.com>
2026-03-20 11:53:13 +0100

    Add files via upload

---
+++ Models/FormInfos.cs
@@ -0,0 +1,21 @@
+﻿namespace TPLOCAL1.Models
+{
+    public class FormInfos
+    {
+        private DateTime dateDebut;
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Sexe { get; set; }
+        public string Adresse { get; set; }
+        public int CodePostal { get; set; }
+        public string Ville { get; set; }
+        public string AdresseMail { get; set; }
+        public DateTime DateDebut { get => dateDebut; set => dateDebut = value; }
+        public string FormationSuivie { get; set; }
+        public string AvisCobol { get; set; }
+        public string AvisCSharp { get; set; }
+
+        public FormInfos() { }
+    }
+}
